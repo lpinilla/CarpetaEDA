9b0277002e49998558b23089dd6929481dd6af8d
lpinilla <lpinilla@itba.edu.ar>
2018-03-21 10:28:32 -0300

    Added insertion and bubble sort

--- Guia4/Sorting.java
+++ Guia4/Sorting.java
@@ -3,7 +3,9 @@ public class Sorting{
 
 	public static void main(String[] args){
 		int[] array = new int[] { 2 , 1 ,0 ,5 ,6 ,7 ,5, 3, 1 ,9 ,0};
-		selectionSort(array);
+		//selectionSort(array);
+		//insertionSort(array);
+		bubbleSort(array);
 		printArray(array);
 	}
 
@@ -19,6 +21,32 @@ public class Sorting{
 			aux = array[i];
 			array[i] = array[min];
 			array[min] = aux;
+		}
+	}
+
+	public static void insertionSort(int[] array){
+		int aux;
+		for(int i = 0; i < array.length; i++){
+			for(int j = i; j > 0; j--){
+				if(array[j] < array[j-1]){
+					aux = array[j];
+					array[j] = array[j-1];
+					array[j-1] = aux;
+				}
+			}
+		}
+	}
+
+	public static void bubbleSort(int[] array){
+		int aux;
+		for(int i = 0; i < array.length; i++){
+			for(int j= i+1; j < array.length; j++){
+				if(array[i] > array[j]){
+					aux = array[i];
+					array[i] = array[j];
+					array[j] = aux;
+				}
+			}
 		}
 	}
 
