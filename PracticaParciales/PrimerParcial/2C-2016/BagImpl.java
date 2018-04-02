0c979ff5a1bd73d424bef53d531cb0d0dd0cdb16
lpinilla <lpinilla@itba.edu.ar>
2018-04-02 18:43:55 -0300

    corregido bagImpl agregar +20 min total

--- PracticaParciales/PrimerParcial/2C-2016/BagImpl.java
+++ PracticaParciales/PrimerParcial/2C-2016/BagImpl.java
@@ -4,13 +4,13 @@ public class BagImpl<T extends Comparable<T>> implements Bag<T>{
 
 	private class Node<T>{
 		T elem;
-		Node<T> next;
+		Node<T> next, prev;
 		int occurrences;
 
 		public Node(T elem){
 			this.elem = elem;
-			this.next = null;
-			this.occurrences = 0;
+			this.next = this.prev = null;
+			this.occurrences = 1;
 		}
 	}
 	
@@ -23,17 +23,51 @@ public class BagImpl<T extends Comparable<T>> implements Bag<T>{
 
 	public void add(T elem){
 		if(elem == null) throw new IllegalArgumentException();
-		this.root = addR(this.root, elem);
+		this.root = addR(this.root, elem, null);
 	}
 
-	private Node<T> addR(Node<T> curr, T elem){
-		if(curr == null) return new Node<T>(elem);
+	private Node<T> addR(Node<T> curr, T elem, Node<T> prev){
+		if(curr == null) {
+			Node<T> nNode = new Node<T>(elem);
+			nNode.prev = prev;
+			return nNode;
+		}
 		if(curr.elem.compareTo(elem) == 0){
 			curr.occurrences++;
+			curr = checkOccurrences(curr);			
 			return curr;	
 		} 
-		curr.next = addR(curr.next, elem);
+		curr.next = addR(curr.next, elem, curr);
 		return curr;
+	}
+
+	private Node<T> checkOccurrences(Node<T> curr){
+		Node<T> aux = curr.prev;
+		while(aux.prev != null){
+			if(curr.occurrences > aux.occurrences){
+				swapNodes(curr, aux);	
+			}else{
+				aux = aux.prev;
+			}
+		}
+		if(aux.occurrences < curr.occurrences){ //root node
+			swapNodes(aux, curr);
+		}
+		return aux;
+	}
+
+	/*2 opciones: cambiar el valor de los nodos o cambiar
+	**los punteros entre los nodos (8 en total)
+	**la segunda opción es la mejor pero es más complicada
+	**y no vale la pena a menos que T sea un objeto de gran
+	**tamaño*/
+	private void swapNodes(Node<T> n1, Node<T> n2){
+		T aux = n1.elem;
+		n1.elem = n2.elem;
+		n2.elem = aux;
+		int aux2 = n1.occurrences; //cambiar también las repeticiones
+		n1.occurrences = n2.occurrences;
+		n2.occurrences = aux2;
 	}
 
 
